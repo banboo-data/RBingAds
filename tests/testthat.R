eb65cf4a1f8ff7e777fdcf8d049df1cf4aebb72b
jburkhardt <johannes.burkhardt@gmail.com>
2019-08-28 11:13:18 +0200

    start with testtaht

---
+++ tests/testthat.R
@@ -0,0 +1,3 @@
+library(testthat)
+library(RBingAds)
+test_check("RBingAds")
---
+++ tests/testthat/test-date_splitter.R
@@ -0,0 +1,15 @@
+context("date_splitter: splits date into list")
+
+date_list <- .date_splitter(x = "2019-01-01")
+
+test_that("data type is correct", {
+  expect_is(date_list, "list")
+  expect_equal(names(date_list), c("year", "month", "day" ))
+  expect_is(date_list$year, "integer")
+  expect_is(date_list$month, "integer")
+  expect_is(date_list$day, "integer")
+})
+
+
+
+

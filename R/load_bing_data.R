42ab10ef2d9650e095c1be3782834ceb036f9c59
jburkhardt <johannes.burkhardt@gmail.com>
2019-08-29 16:31:47 +0200

    implement function timeout if download url unavailable, fixes #14

--- R/load_bing_data.R
+++ R/load_bing_data.R
@@ -1,6 +1,7 @@
 #' @title Load Bing Data
 #'
 #' @description load_bing_data queries the Bing Ads API and loads the data.
+#' Note: There is a function timeout after ten seconds if the data download url is not available.
 #'
 #' @param bing_auth auth object generated with authenticate()
 #' @param customer_id customer / manager id
@@ -32,30 +33,45 @@ load_bing_data <- function(bing_auth,
                            report,
                            columns,
                            start,
-                           end){
-
+                           end) {
   if (as.numeric(Sys.time()) - 3600 >= bing_auth$access$time_stamp) {
-    bing_auth$access <- .refresh_token(credentials = bing_auth$credentials,
-                                       refresh_token = bing_auth$access$refresh_token)
+    bing_auth$access <-
+      .refresh_token(
+        credentials = bing_auth$credentials,
+        refresh_token = bing_auth$access$refresh_token
+      )
   }
 
-  report_id <- get_report_id(bing_auth = bing_auth,
-                             customer_id = customer_id,
-                             account_id = account_id,
-                             report = report,
-                             columns = columns,
-                             start = start,
-                             end = end)
+  report_id <- get_report_id(
+    bing_auth = bing_auth,
+    customer_id = customer_id,
+    account_id = account_id,
+    report = report,
+    columns = columns,
+    start = start,
+    end = end
+  )
 
   download_url <- get_download_url(bing_auth = bing_auth,
                                    report_id = report_id)
 
-  while(class(download_url) == "XMLAttributes"){
+  start_time <- as.numeric(Sys.time())
+  time_out <- 10
+
+  while (class(download_url) == "XMLAttributes" &
+         as.numeric(Sys.time()) - start_time < time_out) {
     download_url <- get_download_url(bing_auth = bing_auth,
                                      report_id = report_id)
   }
 
-  data <- download_data(download_url = download_url)
+  if (grepl("https", download_url)) {
+    data <- download_data(download_url = download_url)
+    cat(sprintf("data downloaded for account: %s\n", account_id))
+    data
 
-  data
+  } else {
+    cat(sprintf("no data available for account: %s\n", account_id))
+
+  }
+
 }

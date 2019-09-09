b561d3a88db6a979bdecb1761c7a236a1230fb6b
jburkhardt <johannes.burkhardt@gmail.com>
2019-09-09 16:07:14 +0200

    adds code examples to exported functions, fixes #20

--- R/authenticate.R
+++ R/authenticate.R
@@ -1,24 +1,30 @@
 #' @title  Invoke the Authentication Process with Bing
-#'
 #' @description This function starts the authentication process with
 #' Bing. Note that this function needs user interaction.
-#'
 #' @param save logical, should the authentication information should be saved on disk? Defaults to FALSE.
 #' @param path path to auth file directory
-#'
+#' @examples
+#' \dontrun{
+#' bing_auth <- authenticate(save = F)
+#' }
 #' @return data.frame containing credentials, optionally saved as .RData in the current working directory
-#'
 #' @export
 authenticate <- function(save = F,
-                         path = ".bingauth"){
-  if (exists("bing_auth")){
-    bing_auth$access <- .refresh_token(credentials = bing_auth$credentials,
-                                       refresh_token = bing_auth$access$refresh_token)
+                         path = ".bingauth") {
+  if (exists("bing_auth")) {
+    bing_auth$access <-
+      .refresh_token(
+        credentials = bing_auth$credentials,
+        refresh_token = bing_auth$access$refresh_token
+      )
   }
-  else if (file.exists(file.path(path, ".bing.auth.RData"))){
+  else if (file.exists(file.path(path, ".bing.auth.RData"))) {
     load(file.path(path, ".bing.auth.RData"))
-    bing_auth$access <- .refresh_token(credentials = bing_auth$credentials,
-                                       refresh_token = bing_auth$access$refresh_token)
+    bing_auth$access <-
+      .refresh_token(
+        credentials = bing_auth$credentials,
+        refresh_token = bing_auth$access$refresh_token
+      )
   } else {
     credentials <- .get_credentials()
     access_token <- .load_token(credentials)
@@ -29,22 +35,23 @@ authenticate <- function(save = F,
     if (save) {
       if (!dir.exists(paths = path)) {
         dir.create(path = path)
-        }
+      }
       save("bing_auth", file = file.path(path, ".bing.auth.RData"))
       # make sure your credentials are
       # ignored by svn and git
       if (!file.exists(".gitignore")) {
         cat(file.path(path, ".bing.auth.RData"),
             file = ".gitignore",
-            sep = "\n"
-        )
+            sep = "\n")
       }
       if (file.exists(".gitignore")) {
         gitignore <- readLines(".gitignore")
         if (!is.element(file.path(path, ".bing.auth.RData"), gitignore)) {
-          cat(file.path(path, ".bing.auth.RData"),
-              file = ".gitignore",
-              append = TRUE, fill = TRUE
+          cat(
+            file.path(path, ".bing.auth.RData"),
+            file = ".gitignore",
+            append = TRUE,
+            fill = TRUE
           )
         }
       }

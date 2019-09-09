f5d0d69cc257414f1401fea39bfa7c02b5ed370b
jburkhardt <johannes.burkhardt@gmail.com>
2019-09-09 15:58:10 +0200

    omit examples of unexported functions

--- R/download_data.R
+++ R/download_data.R
@@ -1,26 +1,19 @@
 #' @title Download Reporting Data
-#'
 #' @description downloads data from download_url
-#'
 #' @param download_url url created with get_report_url
-#'
 #' @importFrom utils download.file read.csv unzip
-#'
-#' @examples
-#' \dontrun{
-#' data <- download_data(download_url = download_url)
-#' }
-#'
 #' @return data
-download_data <- function(download_url){
- temp_file <- file.path(tempdir(), "temp.zip")
- download.file(url = download_url,
-               destfile = temp_file,
-               mode = 'wb',
-               method ='auto')
- file <- unzip(temp_file, list = TRUE)
- df <- read.csv(unz(temp_file, file$Name), stringsAsFactors = F)
- close(unz(temp_file, file$Name))
- unlink(temp_file)
- df
+download_data <- function(download_url) {
+        temp_file <- file.path(tempdir(), "temp.zip")
+        download.file(
+                url = download_url,
+                destfile = temp_file,
+                mode = 'wb',
+                method = 'auto'
+        )
+        file <- unzip(temp_file, list = TRUE)
+        df <- read.csv(unz(temp_file, file$Name), stringsAsFactors = F)
+        close(unz(temp_file, file$Name))
+        unlink(temp_file)
+        df
 }
--- R/get_report_id.R
+++ R/get_report_id.R
@@ -1,64 +1,77 @@
 #' @title Get Report ID
-#'
 #' @description get_report_id queries the Bing API and request a report id.
-#'
 #' @param bing_auth auth object generated with authenticate()
 #' @param account_id account id
 #' @param report report type
 #' @param columns columns, attributes and metrics
 #' @param start start date
 #' @param end end date
-#'
 #' @importFrom XML xmlToList
 #' @importFrom RCurl curlPerform basicTextGatherer
-#'
-#' @examples
-#' \dontrun{
-#' report_id <- get_report_id(bing_auth = bing_auth,
-#'                          account_id = "*******",
-#'                          report = "CampaignPerformanceReport",
-#'                          columns = c("AccountName", "CampaignName",
-#'                                      "TimePeriod", "Impressions",
-#'                                      "Clicks", "Spend", "Conversions"),
-#'                          start = "2019-01-01",
-#'                          end = "2019-01-31")
-#' }
+#' @return report id
 get_report_id <- function(bing_auth,
                           account_id,
                           report,
                           columns,
                           start,
-                          end){
-
+                          end) {
   start_date <- .date_splitter(start)
   end_date <- .date_splitter(end)
-  url <- "https://reporting.api.bingads.microsoft.com/Api/Advertiser/Reporting/v13/ReportingService.svc"
+  url <-
+    "https://reporting.api.bingads.microsoft.com/Api/Advertiser/Reporting/v13/ReportingService.svc"
   soap_action <- "SubmitGenerateReport"
-  header <- paste(readLines(paste0(system.file(package = "RBingAds"), "/xml/reporting.header.authtokens.xml")), collapse = "")
-  body_xml <- paste(readLines(paste0(system.file(package = "RBingAds"), "/xml/reporting.submitGenerateReportRequest.xml")), collapse = "")
+  header <-
+    paste(readLines(
+      paste0(
+        system.file(package = "RBingAds"),
+        "/xml/reporting.header.authtokens.xml"
+      )
+    ), collapse = "")
+  body_xml <-
+    paste(readLines(
+      paste0(
+        system.file(package = "RBingAds"),
+        "/xml/reporting.submitGenerateReportRequest.xml"
+      )
+    ), collapse = "")
   columns_xml <- .get_columns_xml(report, columns)
 
-  body_xml <- sprintf(body_xml,
-                     report,
-                     columns_xml,
-                     account_id,
-                     end_date$day,
-                     end_date$month,
-                     end_date$year,
-                     start_date$day,
-                     start_date$month,
-                     start_date$year)
+  body_xml <- sprintf(
+    body_xml,
+    report,
+    columns_xml,
+    account_id,
+    end_date$day,
+    end_date$month,
+    end_date$year,
+    start_date$day,
+    start_date$month,
+    start_date$year
+  )
 
-  body <- sprintf(header,
-                  soap_action,
-                  bing_auth$credentials$c_id,
-                  bing_auth$access$access_token,
-                  bing_auth$credentials$auth_developer_token,
-                  body_xml)
+  body <- sprintf(
+    header,
+    soap_action,
+    bing_auth$credentials$c_id,
+    bing_auth$access$access_token,
+    bing_auth$credentials$auth_developer_token,
+    body_xml
+  )
 
-  headerFields =  c(Accept = "text/xml", Accept = "multipart/*", 'Content-Type' = "text/xml;charset=utf-8", SOAPAction = soap_action)
+  headerFields =  c(
+    Accept = "text/xml",
+    Accept = "multipart/*",
+    'Content-Type' = "text/xml;charset=utf-8",
+    SOAPAction = soap_action
+  )
   h = basicTextGatherer()
-  curlPerform(url = url, httpheader = headerFields, postfields = body, writefunction = h$update)
-  reportId <- xmlToList(h$value())$Body$SubmitGenerateReportResponse$ReportRequestId
+  curlPerform(
+    url = url,
+    httpheader = headerFields,
+    postfields = body,
+    writefunction = h$update
+  )
+  reportId <-
+    xmlToList(h$value())$Body$SubmitGenerateReportResponse$ReportRequestId
   return(reportId)
 }
